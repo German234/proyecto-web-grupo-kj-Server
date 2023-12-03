ec9ebd07e44bef632d39e7a72b765a3b9cf1a2f9
German234 <00059122@uca.edu.sv>
2023-12-03 17:54:39 -0600

    Fix: fixing problems with the database connection :bug:

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ const morgan = require("morgan");
 const cors = require("cors");
 
 const app = express();
+database.connect();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 app.use(cors());
 app.use("/api/v1", mainRouter);
 app.use(errorHandler);
-
-database.connect();
 
 const port = envconfig.PORT;
 
