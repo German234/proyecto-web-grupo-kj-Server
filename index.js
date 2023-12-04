4268855f345f6804e91b7df2767c37266973a6fd
German234 <00059122@uca.edu.sv>
2023-12-03 18:16:05 -0600

    fix: fixing listener and connection

--- index.js
+++ index.js
@@ -10,17 +10,31 @@ const morgan = require("morgan");
 const cors = require("cors");
 
 const app = express();
-database.connect();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 app.use(cors());
 app.use("/api/v1", mainRouter);
+
+app.use("/", (req, res) => {
+  res.send("Welcome to CSS API");
+});
+
 app.use(errorHandler);
 
 const port = envconfig.PORT;
 
-app.listen(port, () => {
-  debug(`Server is running on port ${port}`);
-});
+const startServer = () => {
+  try {
+    database.connect();
+
+    app.listen(port, () => {
+      debug(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
